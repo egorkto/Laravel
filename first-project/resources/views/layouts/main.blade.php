fad0770131e582e35829a003e53ebf9f6103c024
Egor <69670594+egorkto@users.noreply.github.com>
2025-01-11 23:38:13 +0300

    add bootstrap

--- first-project/resources/views/layouts/main.blade.php
+++ first-project/resources/views/layouts/main.blade.php
@@ -3,24 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
+    @vite('resources/js/app.js')
     <title>Posts</title>
 </head>
 <body>
     <div class="container">
         <div class="row">
-            <nav>
-                <ul>
-                    <li><a href="{{ route('main.index') }}">Main</a></li>
-                    <li><a href="{{ route('posts.index') }}">Posts</a></li>
-                    <li><a href="{{ route('about.index') }}">About</a></li>
-                    <li><a href="{{ route('contacts.index') }}">Contacts</a></li>
-                </ul>
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="{{ route('main.index') }}">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('posts.index') }}">Posts</a>
+                                </li>
+                             <li class="nav-item">
+                                <a class="nav-link" href="{{ route('contacts.index') }}">Contacts</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" aria-disabled="true" href="{{ route('about.index') }}">About</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </nav>
         </div>
-    </div>
-    <div>
-        @yield('content')
+        <div class="row">
+            @yield('content')
+        </div>
     </div>
 </body>
 </html>
