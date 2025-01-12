91844e8611c4f0ddf50b0be3b4e4f72441fcbe04
Egor <69670594+egorkto@users.noreply.github.com>
2025-01-12 21:57:21 +0300

    add requests

--- first-project/app/Http/Controllers/Post/StoreController.php
+++ first-project/app/Http/Controllers/Post/StoreController.php
@@ -3,18 +3,14 @@
 namespace App\Http\Controllers\Post;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Post\StoreRequest;
 use App\Models\Post;
 
 class StoreController extends Controller
 {
-    public function __invoke()
+    public function __invoke(StoreRequest $request)
     {
-        $data = request()->validate([
-            'title' => 'required|string',
-            'content' => 'required|string',
-            'category_id' => '',
-            'tag_ids' => '',
-        ]);
+        $data = $request->validated();
         $tags = $data['tag_ids'];
         unset($data['tag_ids']);
 
--- first-project/app/Http/Controllers/Post/UpdateController.php
+++ first-project/app/Http/Controllers/Post/UpdateController.php
@@ -3,18 +3,14 @@
 namespace App\Http\Controllers\Post;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Post\UpdateRequest;
 use App\Models\Post;
 
 class UpdateController extends Controller
 {
-    public function __invoke(Post $post)
+    public function __invoke(UpdateRequest $request, Post $post)
     {
-        $data = request()->validate([
-            'title' => 'required|string',
-            'content' => 'string',
-            'category_id' => '',
-            'tag_ids' => '',
-        ]);
+        $data = $request->validated();
         $tags = $data['tag_ids'];
         unset($data['tag_ids']);
 
